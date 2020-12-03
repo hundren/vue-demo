4e8e1ec49aa4aea04975d906f188cef6cbf469ad
hugo <hundren@qq.com>
2020-12-03 20:47:13 +0800

    feat: 表单组件库

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <HelloWorld msg="shixx19最漂亮"/>
+    <div class="switch-box">
+      <el-switch
+        v-model="isEdit"
+        active-text="表单"
+        inactive-text="详情">
+      </el-switch>
+    </div>
+    <div class="container">
+      <FormBox v-if="isEdit" v-model="form"/>
+      <Detail v-else :form="form"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormBox from './components/Form.vue'
+import Detail from './components/Detail.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    FormBox,
+    Detail
+  },
+  data() {
+    return {
+      isEdit: true,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
 }
 </script>
 
@@ -20,8 +47,15 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container{
+  width: 60%;
+  margin: 0 auto;
+}
+.switch-box{
+  text-align: center;
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/components/Detail.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px" size="mini">
+      <el-form-item label="活动名称">
+        {{form.name || '暂无输入'}}
+      </el-form-item>
+      <el-form-item label="活动区域">
+        {{form.region | regionMap}}
+      </el-form-item>
+      <el-form-item label="活动时间">
+        {{form.date1}} - {{form.date2}}
+      </el-form-item>
+      <el-form-item label="即时配送">
+        {{form.delivery?'是':'否'}}
+      </el-form-item>
+      <el-form-item label="活动性质">
+        {{form.type | typeMap}}
+      </el-form-item>
+      <el-form-item label="特殊资源">
+        {{form.resource || '暂无选择'}}
+      </el-form-item>
+      <el-form-item label="活动形式">
+        {{form.desc || '暂无输入'}}
+      </el-form-item>
+    </el-form>
+</template>
+<script>
+export default {
+  props:{
+    form:{
+      type: Object,
+      default: ()=> {
+        return{
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    }
+  },
+  filters:{
+    typeMap: function(type){
+        if(type.length > 0){
+          return type.join(',')
+        }else{
+          return '暂没选择'
+        }
+    },
+    regionMap: function(region){
+      const map = {
+        beijing :'区域二',
+        shanghai:'区域一' 
+      }
+      return map[region] || '暂无选择'
+    }
+  }
+}
+</script>
